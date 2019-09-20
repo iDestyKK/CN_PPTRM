b671f59427422aa3292e3854b9fda6fee21a4f4f
iDestyKK <clara@claranguyen.me>
2019-09-20 17:02:16 -0400

    Adjusted changelog (again...)

--- CN_PPTRM/about.Designer.cs
+++ CN_PPTRM/about.Designer.cs
@@ -70,7 +70,7 @@
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Location = new System.Drawing.Point(12, 124);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(440, 246);
+            this.groupBox2.Size = new System.Drawing.Size(440, 235);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Tool Details";
@@ -84,7 +84,7 @@
             this.richTextBox1.Location = new System.Drawing.Point(10, 77);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.ReadOnly = true;
-            this.richTextBox1.Size = new System.Drawing.Size(424, 163);
+            this.richTextBox1.Size = new System.Drawing.Size(424, 152);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
             // 
@@ -102,7 +102,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(464, 382);
+            this.ClientSize = new System.Drawing.Size(464, 371);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
