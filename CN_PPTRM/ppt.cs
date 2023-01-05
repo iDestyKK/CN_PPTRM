df60bcd9918835774b2b05382df06cac969b1bed
iDestyKK <clara@claranguyen.me>
2023-01-05 00:28:21 -0500

    Deduplication

--- CN_PPTRM/ppt.cs
+++ CN_PPTRM/ppt.cs
@@ -55,11 +55,157 @@ namespace ppt_replay_gui {
             public abstract string get_length_as_string(uint id);
 
             //Other things
-            public abstract void fill_tree(TreeView tv);
-            public abstract byte[] generate_dem(uint id);
-            public abstract void import_dem(byte[] dem, uint id);
-            public abstract string generate_hexdump(uint offset, uint len);
-            public abstract void erase(uint id);
+
+            /*
+             * data_bin.fill_tree
+             * 
+             * Fills in a TreeView with the replays available in this object.
+             * Clicking those nodes will let you view data about each replay,
+             * and even export them as a file, or replace them with another
+             * replay, if possible.
+             */
+
+            public void fill_tree(TreeView tv)
+            {
+                //Clear out TreeView...
+                tv.Nodes.Clear();
+
+                //Recreate and fill in with actual data.
+                TreeNode root = tv.Nodes.Add("Save Data");
+                root.Name = "treeNode_saveData";
+
+                for (uint i = 0; i < replay_count; i++)
+                {
+                    TreeNode n = new TreeNode(i.ToString("00") + ": " + time_format(i) + ".dem");
+                    n.Name = i.ToString("00");
+                    root.Nodes.Add(n);
+                }
+            }
+
+            /*
+             * data_bin.generate_dem
+             * 
+             * Generates an array of bytes that can be extracted as a valid DEM
+             * file. This is essentially concatenating the PREP and DATA
+             * sections together.
+             */
+
+            public byte[] generate_dem(uint id)
+            {
+                byte[] dem_buffer = new byte[PREP_LEN + DATA_LEN];
+                Buffer.BlockCopy(bytes, (int)PREP_ADDR[id], dem_buffer, 0x00, (int)PREP_LEN);
+                Buffer.BlockCopy(bytes, (int)DATA_ADDR[id], dem_buffer, (int)PREP_LEN, (int)DATA_LEN);
+
+                return dem_buffer;
+            }
+
+            /*
+             * data_bin.import_dem
+             * 
+             * Imports a DEM file's bytes from "dem" and injects it into the
+             * save, replacing a replay at position "id". This is essentially
+             * the inverse of "generate_dem", as we are importing data rather
+             * than exporting it.
+             */
+
+            public void import_dem(byte[] dem, uint id)
+            {
+                Buffer.BlockCopy(dem, 0x00, bytes, (int)PREP_ADDR[id], (int)PREP_LEN);
+                Buffer.BlockCopy(dem, (int)PREP_LEN, bytes, (int)DATA_ADDR[id], (int)DATA_LEN);
+            }
+
+            /*
+             * data_bin.generate_hexdump
+             * 
+             * Generates a string with a hexdump mimicing the POSIX command
+             * "hexdump -Cv FILE". This can be used to aid in figuring out some
+             * other values in the save data... Or, you know, just to dump more
+             * information for the end user to see.
+             */
+
+            public string generate_hexdump(uint offset, uint len)
+            {
+                string hexdump;
+                uint shift, rows, addr, target;
+
+                //Print out bytes... 16 values per row, starting where the
+                //section should begin at...
+                hexdump = "";
+                shift = (16 - (offset % 16)) % 16;
+                rows = (uint)Math.Ceiling((double)(len + shift) / 16);
+                addr = offset - shift;
+                target = addr + len + shift;
+
+                hexdump += "           00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F" + Environment.NewLine;
+                hexdump += "          _________________________________________________" + Environment.NewLine;
+
+                for (int r = 0; r < rows; r++)
+                {
+                    hexdump += string.Format("{0:X8} | ", addr);
+
+                    //Hex Values
+                    for (int c = 0; c < 16; c++)
+                    {
+                        if (addr + c < offset || addr + c >= offset + len)
+                            hexdump += "   ";
+                        else
+                            hexdump += string.Format("{0:X2} ", bytes[addr + c]);
+                    }
+
+                    //Plain Text
+                    hexdump += "|";
+
+                    for (int c = 0; c < 16; c++)
+                    {
+                        if (addr + c < offset || addr + c >= offset + len)
+                            hexdump += " ";
+                        else
+                        if (!Char.IsControl((char)bytes[addr + c]))
+                            hexdump += (char)bytes[addr + c];
+                        else
+                            hexdump += ".";
+                    }
+
+                    hexdump += "|" + Environment.NewLine;
+                    addr += 0x10;
+                }
+
+                return hexdump;
+            }
+
+            //Linked-List-like Functionality
+
+            /*
+             * data_bin.erase
+             * 
+             * Erases a replay in the save. This will also move all sequential
+             * replays up by 1.
+             */
+
+            public void erase(uint id)
+            {
+                //Copy blocks of data over
+                if (id != 49)
+                {
+                    Buffer.BlockCopy(
+                        bytes, (int)PREP_ADDR[id + 1],
+                        bytes, (int)PREP_ADDR[id],
+                        (int)PREP_LEN * (50 - ((int)id + 1))
+                    );
+
+                    Buffer.BlockCopy(
+                        bytes, (int)DATA_ADDR[id + 1],
+                        bytes, (int)DATA_ADDR[id],
+                        (int)DATA_LEN * (50 - ((int)id + 1))
+                    );
+                }
+
+                //Zero out the 50th PREP and DATA spots.
+                Array.Clear(bytes, (int)PREP_ADDR[49], (int)PREP_LEN);
+                Array.Clear(bytes, (int)DATA_ADDR[49], (int)DATA_LEN);
+
+                replay_count--;
+            }
         }
 
         public class data_bin_ppt1_pc : data_bin {
@@ -187,147 +333,6 @@ namespace ppt_replay_gui {
                 sec = duration % 60;
 
                 return min.ToString() + ":" + sec.ToString("00");
-            }
-
-            /*
-             * data_bin.fill_tree
-             * 
-             * Fills in a TreeView with the replays available in this object.
-             * Clicking those nodes will let you view data about each replay,
-             * and even export them as a file, or replace them with another
-             * replay, if possible.
-             */
-
-            public override void fill_tree(TreeView tv) {
-                //Clear out TreeView...
-                tv.Nodes.Clear();
-
-                //Recreate and fill in with actual data.
-                TreeNode root = tv.Nodes.Add("Save Data");
-                root.Name = "treeNode_saveData";
-
-                for (uint i = 0; i < replay_count; i++) {
-                    TreeNode n = new TreeNode(i.ToString("00") + ": " + time_format(i) + ".dem");
-                    n.Name = i.ToString("00");
-                    root.Nodes.Add(n);
-                }
-            }
-
-            /*
-             * data_bin.generate_dem
-             * 
-             * Generates an array of bytes that can be extracted as a valid DEM
-             * file. This is essentially concatenating the PREP and DATA
-             * sections together.
-             */
-
-            public override byte[] generate_dem(uint id) {
-                byte[] dem_buffer = new byte[PREP_LEN + DATA_LEN];
-                Buffer.BlockCopy(bytes, (int)PREP_ADDR[id], dem_buffer, 0x00, (int)PREP_LEN);
-                Buffer.BlockCopy(bytes, (int)DATA_ADDR[id], dem_buffer, (int)PREP_LEN, (int)DATA_LEN);
-
-                return dem_buffer;
-            }
-
-            /*
-             * data_bin.import_dem
-             * 
-             * Imports a DEM file's bytes from "dem" and injects it into the
-             * save, replacing a replay at position "id". This is essentially
-             * the inverse of "generate_dem", as we are importing data rather
-             * than exporting it.
-             */
-
-            public override void import_dem(byte[] dem, uint id) {
-                Buffer.BlockCopy(dem, 0x00, bytes, (int)PREP_ADDR[id], (int)PREP_LEN);
-                Buffer.BlockCopy(dem, (int)PREP_LEN, bytes, (int)DATA_ADDR[id], (int)DATA_LEN);
-            }
-
-            /*
-             * data_bin.generate_hexdump
-             * 
-             * Generates a string with a hexdump mimicing the POSIX command
-             * "hexdump -Cv FILE". This can be used to aid in figuring out some
-             * other values in the save data... Or, you know, just to dump more
-             * information for the end user to see.
-             */
-
-            public override string generate_hexdump(uint offset, uint len) {
-                string hexdump;
-                uint shift, rows, addr, target;
-
-                //Print out bytes... 16 values per row, starting where the
-                //section should begin at...
-                hexdump = "";
-                shift = (16 - (offset % 16)) % 16;
-                rows = (uint)Math.Ceiling((double)(len + shift) / 16);
-                addr = offset - shift;
-                target = addr + len + shift;
-
-                hexdump += "           00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F" + Environment.NewLine;
-                hexdump += "          _________________________________________________" + Environment.NewLine;
-
-                for (int r = 0; r < rows; r++) {
-                    hexdump += string.Format("{0:X8} | ", addr);
-
-                    //Hex Values
-                    for (int c = 0; c < 16; c++) {
-                        if (addr + c < offset || addr + c >= offset + len)
-                            hexdump += "   ";
-                        else
-                            hexdump += string.Format("{0:X2} ", bytes[addr + c]);
-                    }
-
-                    //Plain Text
-                    hexdump += "|";
-
-                    for (int c = 0; c < 16; c++) {
-                        if (addr + c < offset || addr + c >= offset + len)
-                            hexdump += " ";
-                        else
-                        if (!Char.IsControl((char)bytes[addr + c]))
-                            hexdump += (char)bytes[addr + c];
-                        else
-                            hexdump += ".";
-                    }
-
-                    hexdump += "|" + Environment.NewLine;
-                    addr += 0x10;
-                }
-
-                return hexdump;
-            }
-
-            //Linked-List-like Functionality
-
-            /*
-             * data_bin.erase
-             * 
-             * Erases a replay in the save. This will also move all sequential
-             * replays up by 1.
-             */
-
-            public override void erase(uint id) {
-                //Copy blocks of data over
-                if (id != 49) {
-                    Buffer.BlockCopy(
-                        bytes, (int)PREP_ADDR[id + 1],
-                        bytes, (int)PREP_ADDR[id],
-                        (int)PREP_LEN * (50 - ((int)id + 1))
-                    );
-
-                    Buffer.BlockCopy(
-                        bytes, (int)DATA_ADDR[id + 1],
-                        bytes, (int)DATA_ADDR[id],
-                        (int)DATA_LEN * (50 - ((int)id + 1))
-                    );
-                }
-
-                //Zero out the 50th PREP and DATA spots.
-                Array.Clear(bytes, (int)PREP_ADDR[49], (int)PREP_LEN);
-                Array.Clear(bytes, (int)DATA_ADDR[49], (int)DATA_LEN);
-
-                replay_count--;
             }
         }
 
@@ -473,157 +478,6 @@ namespace ppt_replay_gui {
                 sec = duration % 60;
 
                 return min.ToString() + ":" + sec.ToString("00");
-            }
-
-            /*
-             * data_bin.fill_tree
-             * 
-             * Fills in a TreeView with the replays available in this object.
-             * Clicking those nodes will let you view data about each replay,
-             * and even export them as a file, or replace them with another
-             * replay, if possible.
-             */
-
-            public override void fill_tree(TreeView tv)
-            {
-                //Clear out TreeView...
-                tv.Nodes.Clear();
-
-                //Recreate and fill in with actual data.
-                TreeNode root = tv.Nodes.Add("Save Data");
-                root.Name = "treeNode_saveData";
-
-                for (uint i = 0; i < replay_count; i++)
-                {
-                    TreeNode n = new TreeNode(i.ToString("00") + ": " + time_format(i) + ".dem");
-                    n.Name = i.ToString("00");
-                    root.Nodes.Add(n);
-                }
-            }
-
-            /*
-             * data_bin.generate_dem
-             * 
-             * Generates an array of bytes that can be extracted as a valid DEM
-             * file. This is essentially concatenating the PREP and DATA
-             * sections together.
-             */
-
-            public override byte[] generate_dem(uint id)
-            {
-                byte[] dem_buffer = new byte[PREP_LEN + DATA_LEN];
-                Buffer.BlockCopy(bytes, (int)PREP_ADDR[id], dem_buffer, 0x00, (int)PREP_LEN);
-                Buffer.BlockCopy(bytes, (int)DATA_ADDR[id], dem_buffer, (int)PREP_LEN, (int)DATA_LEN);
-
-                return dem_buffer;
-            }
-
-            /*
-             * data_bin.import_dem
-             * 
-             * Imports a DEM file's bytes from "dem" and injects it into the
-             * save, replacing a replay at position "id". This is essentially
-             * the inverse of "generate_dem", as we are importing data rather
-             * than exporting it.
-             */
-
-            public override void import_dem(byte[] dem, uint id)
-            {
-                Buffer.BlockCopy(dem, 0x00, bytes, (int)PREP_ADDR[id], (int)PREP_LEN);
-                Buffer.BlockCopy(dem, (int)PREP_LEN, bytes, (int)DATA_ADDR[id], (int)DATA_LEN);
-            }
-
-            /*
-             * data_bin.generate_hexdump
-             * 
-             * Generates a string with a hexdump mimicing the POSIX command
-             * "hexdump -Cv FILE". This can be used to aid in figuring out some
-             * other values in the save data... Or, you know, just to dump more
-             * information for the end user to see.
-             */
-
-            public override string generate_hexdump(uint offset, uint len)
-            {
-                string hexdump;
-                uint shift, rows, addr, target;
-
-                //Print out bytes... 16 values per row, starting where the
-                //section should begin at...
-                hexdump = "";
-                shift = (16 - (offset % 16)) % 16;
-                rows = (uint)Math.Ceiling((double)(len + shift) / 16);
-                addr = offset - shift;
-                target = addr + len + shift;
-
-                hexdump += "           00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F" + Environment.NewLine;
-                hexdump += "          _________________________________________________" + Environment.NewLine;
-
-                for (int r = 0; r < rows; r++)
-                {
-                    hexdump += string.Format("{0:X8} | ", addr);
-
-                    //Hex Values
-                    for (int c = 0; c < 16; c++)
-                    {
-                        if (addr + c < offset || addr + c >= offset + len)
-                            hexdump += "   ";
-                        else
-                            hexdump += string.Format("{0:X2} ", bytes[addr + c]);
-                    }
-
-                    //Plain Text
-                    hexdump += "|";
-
-                    for (int c = 0; c < 16; c++)
-                    {
-                        if (addr + c < offset || addr + c >= offset + len)
-                            hexdump += " ";
-                        else
-                        if (!Char.IsControl((char)bytes[addr + c]))
-                            hexdump += (char)bytes[addr + c];
-                        else
-                            hexdump += ".";
-                    }
-
-                    hexdump += "|" + Environment.NewLine;
-                    addr += 0x10;
-                }
-
-                return hexdump;
-            }
-
-            //Linked-List-like Functionality
-
-            /*
-             * data_bin.erase
-             * 
-             * Erases a replay in the save. This will also move all sequential
-             * replays up by 1.
-             */
-
-            public override void erase(uint id)
-            {
-                //Copy blocks of data over
-                if (id != 49)
-                {
-                    Buffer.BlockCopy(
-                        bytes, (int)PREP_ADDR[id + 1],
-                        bytes, (int)PREP_ADDR[id],
-                        (int)PREP_LEN * (50 - ((int)id + 1))
-                    );
-
-                    Buffer.BlockCopy(
-                        bytes, (int)DATA_ADDR[id + 1],
-                        bytes, (int)DATA_ADDR[id],
-                        (int)DATA_LEN * (50 - ((int)id + 1))
-                    );
-                }
-
-                //Zero out the 50th PREP and DATA spots.
-                Array.Clear(bytes, (int)PREP_ADDR[49], (int)PREP_LEN);
-                Array.Clear(bytes, (int)DATA_ADDR[49], (int)DATA_LEN);
-
-                replay_count--;
             }
         }
     }
